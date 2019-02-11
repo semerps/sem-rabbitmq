84744d782db623d894a3a09f504c3cee8f9d7606
Aydın Öztürk <aydinozturk41@gmail.com>
2019-02-11 15:30:43 +0300

    Conf Fix

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,4 +1,8 @@
-grails.project.work.dir = 'target'
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+
+grails.project.dependency.resolver = "maven"
 
 grails.project.dependency.resolution = {
     inherits 'global'
@@ -28,3 +32,4 @@ grails.project.dependency.resolution = {
         }
     }
 }
+
